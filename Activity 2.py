5bdea2fc10847c06069e64121e6d9ff19fdfd8bc
Parsz123 <hetaldr@yahoo.com>
2025-11-23 11:17:07 +0000

    hello

---
+++ Activity 2.py
@@ -0,0 +1,23 @@
+def palind(r):
+    e = len(r)-1
+    s = 0
+    while (s<e):
+        if r[s]!= r[e]:
+            return False
+        s+= 1
+        e-=1
+    return True
+
+r = (1,2,3,3,2,1)
+
+if palind(r):
+    print("The tuple is flip-flop")
+else:
+    print("The tuple is not flip-flop")
+
+r = (1,6,7,3,2,5)
+
+if palind(r):
+    print("The tuple is flip-flop")
+else:
+    print("The tuple is not flip-flop")
